3f431abadf0eb16fe38f5f03d9f4ff38c1ddb388
DouglasAmarelo <d.lopes.nascimento@gmail.com>
2019-08-10 22:15:28 -0300

    - feat: :bulb: Start front-end project - refactor: :wrench: Reorganização de arquivos e pacotes

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const routes = require('./routes');
 const cors = require('cors');
 
+
 const server = express();
 
 mongoose.connect('mongodb+srv://admindatabase:admindatabase@cluster0-sisl5.mongodb.net/omnistack8?retryWrites=true&w=majority', {
